abe7308a2dac9944bca38bb95f14a0805950de66
Vay134 <71159266+Vay134@users.noreply.github.com>
2026-07-15 17:51:08 +0800

    feat(frontend): Implemented summaryApi.ts

--- DAS_7/frontend/src/api/summaryApi.ts
+++ DAS_7/frontend/src/api/summaryApi.ts
@@ -1,1 +1,16 @@
-import {createApiUrl} from ../
+import {createApiUrl} from "../config/api";
+
+type SummaryResponse = {
+    studentName: string;
+    summary: string;
+};
+
+export async function getSummary(studentId: string): Promise<SummaryResponse> {
+    const response = await fetch(createApiUrl(`students/{studentId}/summary`));
+
+    if (!response.ok) {
+        throw new Error(`Unable to load users: ${response.status}`);
+    }
+    const data: SummaryResponse = await response.json();
+    return data;
+}
